6c2f4ec457d3a26df1dddcc606f785539faa0cd6
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2026-01-05 09:56:58 +0700

    small fix

--- models/multimodal_retriever/stage3_multimodal_retriever/train_multimodal_biencoder.sh
+++ models/multimodal_retriever/stage3_multimodal_retriever/train_multimodal_biencoder.sh
@@ -1,5 +1,7 @@
 python models/multimodal_retriever/stage3_multimodal_retriever/train_multimodal_biencoder.py \
     --data data/augmented_data/augmented_train_spdocvqa.json \
+    --text_model openai/clip-vit-base-patch32 \
+    --image_model openai/clip-vit-base-patch32 \
     --epochs 20 \
     --hard_mining_every 2 \
     --hard_topm 50 \
