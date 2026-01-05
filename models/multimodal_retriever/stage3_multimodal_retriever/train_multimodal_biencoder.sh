a9c61a8fb8097897db5d6a38fba19f7bc0b8af2f
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2026-01-05 09:52:53 +0700

    create train multimodal biencoder.sh

---
+++ models/multimodal_retriever/stage3_multimodal_retriever/train_multimodal_biencoder.sh
@@ -0,0 +1,8 @@
+python models/multimodal_retriever/stage3_multimodal_retriever/train_biencoder.py \
+    --data data/augmented_data/augmented_train_spdocvqa.json \
+    --epochs 20 \
+    --hard_mining_every 2 \
+    --hard_topm 50 \
+    --hard_neg_k 3 \
+    --query_view full \
+    --index_type flat
