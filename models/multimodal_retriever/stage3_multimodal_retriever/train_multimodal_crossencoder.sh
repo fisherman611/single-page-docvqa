b2f9c8c13d63c5f9ce3f6ba42ca64aa2b356c24d
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2026-02-08 21:26:19 +0700

    Delete some files

--- models/multimodal_retriever/stage3_multimodal_retriever/train_multimodal_crossencoder.sh
+++
@@ -1,4 +0,0 @@
-python models/multimodal_retriever/stage3_multimodal_retriever/train_multimodal_crossencoder.py \
-    --data data/augmented_data/augmented_train_spdocvqa.json \
-    --epochs 5 \
-    --negatives_per_query 3
